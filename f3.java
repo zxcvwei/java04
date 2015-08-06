db87a01c935e08e059f7ef84e6b5d61478ab997c
zxcvwei <104021031@live.asia.edu.tw>
2015-08-06 14:51:49 +0800

    java04
    for

---
+++ f3.java
@@ -0,0 +1,12 @@
+
+public class f3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for(int i = 13;i<=1399;i = i+2){
+			sum = sum+i;
+		}
+		System.out.println("sum="+sum);
+	}
+
+}
