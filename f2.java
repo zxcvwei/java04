0413d8aa620225fc135b10dce3da8e962ec55a10
zxcvwei <104021031@live.asia.edu.tw>
2015-08-06 14:49:42 +0800

    java04
    v1.0

--- f2.java
+++ f2.java
@@ -2,15 +2,11 @@
 public class f2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
 		int sum = 0;
 		for(int i = 13;i<=1399;i++){
 			sum = sum+i;
-			
 		}
 		System.out.println("sum="+sum);
-		
 		
 	}
 
